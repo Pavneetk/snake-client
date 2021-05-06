a590de203909c438201f2b076fb64716339f37d7
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-06 00:41:04 +0000

    sending key inputs to server by passing connection and using write

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,7 +18,7 @@ const setupInput = function () {
       case 's': key = 'Move: down'; break;
       case 'd': key = 'Move: right'; break;
     }
-    process.stdout.write(key);
+    connection.write(key);
   
   });
 
--- play.js
+++ play.js
@@ -9,5 +9,4 @@ const {setupInput} = require('./input');
 
 
 console.log("Connecting ...");
-connect();
-setupInput();
+setupInput(connect());
